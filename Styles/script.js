e9cc8b2073ccc14c40bfcdf6bff0f8313768d5a0
Moritz Amelang <moritz@moritzs-mbp.ecx.local>
2020-07-30 10:48:05 +0200

    nav opacity function

--- Styles/script.js
+++ Styles/script.js
@@ -1,7 +1,13 @@
 const items = document.getElementsByClassName('selfIcon');
 const bar = document.getElementsByClassName('navList');
+const op = document.getElementsByClassName('mainNav')
 
     items[0].addEventListener('click', function(){
 
         bar[0].classList.toggle('is-active'); 
+    });
+
+    items[0].addEventListener('click', function(){
+
+        op[0].classList.toggle('is-active'); 
     });
